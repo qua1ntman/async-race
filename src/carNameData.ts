5c90f9f5a1f342395061e6a55bdcb029d85efc75
Mikhail Ashla <redikfrost@gmail.com>
2022-08-01 16:48:34 +0300

    feat: add helper functions, images, data for randomly generating car names

---
+++ src/carNameData.ts
@@ -0,0 +1,4 @@
+export const carData = {
+  carBrend: ['Toyota', 'Hyundai', 'Kia', 'BMW', 'Audi', 'Mercedes‑Benz', 'Bugatti', 'Buick', 'Tesla', 'Mazda'],
+  carModel: ['Urban', 'Palisade', 'M3', 'Sportage', 'Model 3', 'A‑Class', 'Genesis', 'LX', 'Civic', 'Challenger'],
+};
